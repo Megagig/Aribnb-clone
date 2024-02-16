25e4ff1a7344cfea2a7a07b3f7f0114a0442d95c
megagig <megagigsoftwaresolutions@gmail.comm>
2024-02-16 21:05:47 +0100

    Add the Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const Hero = () => {
+  return (
+    <section className="hero">
+      <img src="./images/Group 77.png" alt="logo" className="Hero-Image" />
+      <h1 className="hero-header">Online Experiences</h1>
+      <p className="hero-text">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  );
+};
+export default Hero;
